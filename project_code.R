6abd006688517e22b9ed1b97ae1bca6078f37357
Brett Erwin <78919870+brett-erwin@users.noreply.github.com>
2021-12-09 20:39:36 -0600

    Update project_code.R

--- project_code.R
+++ project_code.R
@@ -148,7 +148,7 @@ question_1b<-alldatamerged %>%
   summarise(south_atlantic_mean_cli=mean(cli))
 
 
-# Plot visualizing average CLI by region
+# Bar plot visualizing average CLI by region
 barplot(meanCliByRegion$x,xlab="CLI", main="CLI By Region", names.arg=meanCliByRegion$Group.1, horiz=T, las=1, col=c("darkgray","darkgray","darkgray",
                                                                                                                      "darkgray","red","darkgray",
                                                                                                                      "darkgray","darkgray","darkgray"))
